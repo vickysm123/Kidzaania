e1f345822d22ce60637e938c17e0480334196f34
viveksworkspace <teamadsdev22@gmail.com>
2024-11-11 10:57:03 +0530

    b

--- script.js
+++ script.js
@@ -129,3 +129,20 @@ function moveCarousels(direct) {
 
   tracks.style.transform = `translateX(-${currentIndexes * itemWidths}px)`;
 }
+// Back to Top Button
+const backToTopButton = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) { // Show after scrolling 200px
+        backToTopButton.classList.add('show');
+    } else {
+        backToTopButton.classList.remove('show');
+    }
+});
+
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
